6b4a0363a6185ce3b9378d783b0caf2cc0a404e2
setouchi-h <setouchi.kazuki@gmail.com>
2023-10-13 07:23:43 +0900

    update: user

--- root-frontend/vite.config.ts
+++ root-frontend/vite.config.ts
@@ -1,38 +1,27 @@
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill"
-import * as path from "path"
+import { defineConfig } from "vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "src/": path.join(__dirname, "src/"),
-      stream: "rollup-plugin-node-polyfills/polyfills/stream",
-      _stream_duplex: "rollup-plugin-node-polyfills/polyfills/readable-stream/duplex",
-      _stream_passthrough: "rollup-plugin-node-polyfills/polyfills/readable-stream/passthrough",
-      _stream_readable: "rollup-plugin-node-polyfills/polyfills/readable-stream/readable",
-      _stream_writable: "rollup-plugin-node-polyfills/polyfills/readable-stream/writable",
-      _stream_transform: "rollup-plugin-node-polyfills/polyfills/readable-stream/transform",
+      crypto: "empty-module",
+      assert: "empty-module",
+      http: "empty-module",
+      https: "empty-module",
+      os: "empty-module",
+      url: "empty-module",
+      zlib: "empty-module",
+      stream: "stream-browserify",
+      _stream_duplex: "empty-module",
+      _stream_passthrough: "empty-module",
+      _stream_readable: "empty-module",
+      _stream_writable: "empty-module",
+      _stream_transform: "empty-module",
     },
   },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "globalThis",
-      },
-      // Enable esbuild polyfill plugins
-      // https://medium.com/@ftaioli/using-node-js-builtin-modules-with-vite-6194737c2cd2
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          process: true,
-          buffer: true,
-        }),
-      ],
-    },
-  },
-  build: {
-    target: "esnext",
+  define: {
+    global: "globalThis",
   },
 })
